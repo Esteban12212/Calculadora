0602221c113e00c3339e5e77f7dbacfb5161f086
enamo <flexingnenes@gmail.com>
2025-11-20 09:01:39 +0100

    FIRST COMMIT

---
+++ Calculadoragit.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.calculadoragit;
+
+/**
+ *
+ * @author Alumnado
+ */
+public class Calculadoragit {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
